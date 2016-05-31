6d0763d480fbbb35b56c4c410ddddf7acd1ab6ed
Matthew <casqouro@gmail.com>
2016-05-31 15:18:05 -0400

    Adding Initial Project Files
    - Initial project files added
    - Gridboard class requirements added

---
+++ core/src/com/mygdx/game/FarmFlow.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class FarmFlow extends ApplicationAdapter {
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+}
